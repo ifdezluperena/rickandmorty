c22527770ab06cec8e124fd3629cb2daee142e57
Ivan Fernandez <ivan.fdez.luperena@gmail.com>
2023-05-22 21:29:11 +0200

    first and last commit

---
+++ src/types/types.ts
@@ -0,0 +1,21 @@
+export interface Character {
+    name: string;
+    image: string;
+    status: string;
+    species: string;
+    episode:string[];
+}
+
+export interface Episode {
+    id: number;
+    name: string;
+    episode: string;
+}
+
+export interface Location{
+    id:number,
+    name: string,
+    type: string,
+    dimension: string
+    residents:string[]
+}
